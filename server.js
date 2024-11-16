c43e101da2f4be79a4425d935c64ae092c2a7fd3
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2024-11-16 21:21:45 +0100

    change port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 require('./db')
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log('Server running. Use our API on port: 3000')
+app.listen(4000, () => {
+  console.log('Server running. Use our API on port: 4000')
 })
