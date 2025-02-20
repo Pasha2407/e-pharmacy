d8e23f07455ac9023dddf71480fc342ecbbd8f43
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2025-02-20 16:18:30 +0100

    add scss

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { App } from 'components/App/App';
-import './index.css';
+import { App } from './App';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/my-react-template">
+  <BrowserRouter basename="/e-pharmacy">
     <React.StrictMode>
       <App />
     </React.StrictMode>
